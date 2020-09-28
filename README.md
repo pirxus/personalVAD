a4e65895964461528d212709a66b5156603ee493
Simon Sedlacek <saimon.sedlacek@gmail.com>
2020-09-28 20:48:37 +0200

    reworked the dataset generation to utilize word alignments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # personalVAD
 Bachelor's thesis project
+
+## TODO
+- check whether the aligned words really match the final utterances
+- annotate concatenated data - randomly select the target speaker from the three
+- noisify the concatenated utterances to midigate the concatenation artifacts
+        * explore the MTR augmentation approach
+- extract features and store them along with the generated annotations
+- extract ivector for the target speaker and other ivectors for the whole utterance
+- build the model and train it
+        * also, maybe train a baseline VAD model with the same architecture
---
+++ src/concatenate.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+
+import sys
+import os
+from multiprocessing import Process
+import time
+import random
+import math
+import soundfile as sf
+import numpy as np
+from glob import glob
+
+ALIGNED = True
+MPROCESS = True
+N = 20000
+
+
+# This function creates a list that contains tuples of the paths to the utterances
+# and their actual full transcriptions and alignments
+# (full_path, name, aligned_text, tstamps)
+def parse_alignments(path):
+    transcripts = [] 
+    directory = path.rpartition('/')[0] + '/'
+    f = open(path)
+    for line in f.readlines():
+        name = line.split(' ')[0]
+        full_path = directory + name + '.flac'
+        aligned_text = line.split(' ')[1][1:-1]
+        tstamps = line.split(' ')[2][1:-2] # without the newline..
+
+        # store the aligned transcript in the list
+        transcripts.append((full_path, name, aligned_text, tstamps))
+
+    f.close()
+    return transcripts
+
+def load_dataset_structure(root_dir):
+    utterances = []
+    with os.scandir(root) as speakers:
+        for speaker in speakers:
+            #print(f'Now processing the speaker {speaker.name}')
+
+            # extract the paths to the individual files and their transcriptions
+            transcripts = []
+            for dirName, subdirList, fileList in os.walk(root + speaker.name):
+                #print(f'Entering the {dirName} directory')
+        
+                # load the paths to the files and their transcriptions
+                if fileList != []:
+                    for f in glob(dirName + '/*.txt'):
+
+                        # extract the transcriptions and store them in a list
+                        if f.split('.')[-2] == 'alignment':
+                            transcripts.extend(
+                                    parse_alignments(f))
+
+                        #NOTE: I ended up using the alignment files only, since currently
+                        # they are all I need and contain all the information from the
+                        # original transcript files.
+
+            utterances.append((speaker.name, transcripts))
+
+    return utterances
+
+# Trims the end of the utterance so that the alignment timestamps for the other
+# utterances can be offset by exactly n frames and so that the utterance's length is
+# divisible by 10ms
+def trim_utt_end(x, sr, tstamps):
+    end_stamp = math.trunc(float(tstamps[-1]) * 100) / 100
+    end = end_stamp * sr
+    if x.size != end:
+        assert (x.size > end), "Signal length was smaller than the end timestamp"
+        x = x[:int(end-x.size)]
+
+    return x, end_stamp
+
+
+def generate_concatenations(dataset, dest, proc_name='', n=1300):
+    if proc_name != '':
+        print(f'process {proc_name} starting...')
+
+    iteration = 0 # split files into directories by 1000
+    cur_dir = ''
+    while iteration < n:
+        if iteration % 1000 == 0:
+            # create a new destination subdirectory
+            cur_dir = dest + proc_name + str(iteration) + '/'
+            os.mkdir(cur_dir)
+
+        # now randomly select the number of speaker utterances that are to be concatenated
+        n_utter = np.random.randint(1, 4)
+        speakers = random.sample(dataset, n_utter) # randomly select n speakers
+        utterances = []
+        for speaker in speakers: # and for each speaker select a random utterance
+            utterances.append(random.choice(speaker[1]))
+
+        data = np.array([])
+        sr = 16000
+        file_name = ''
+        transcript = ''
+        alignment = ''
+
+        if ALIGNED is True:
+            # concatenate the waveforms, save the new concatenated file and its
+            # transcription and alignment timestamps
+            tstamps = [] 
+            prev_end_stamp = 0
+            for utterance in utterances:
+                x, sr = sf.read(utterance[0])
+                stamps = utterance[3].split(',')
+                x, end_stamp = trim_utt_end(x, sr, stamps)
+                data = np.append(data, x)
+
+                # offset the timestamps
+                if tstamps != []:
+                    tstamps.pop()
+                    tstamps.extend(
+                            [str(round(float(stamp) + prev_end_stamp, 2))
+                                for stamp in stamps])
+                else:
+                    tstamps = stamps
+
+                prev_end_stamp += end_stamp
+
+                file_name += utterance[1] + '_'
+                transcript += utterance[2] + '$'
+
+            alignment = ' '.join(tstamps)
+            file_name = file_name[:-1]
+            transcript = transcript[:-1]
+
+        else:
+
+            # concatenate the waveforms, save the new concatenated file and its
+            # transcription
+            for utterance in utterances:
+                x, sr = sf.read(utterance[0])
+                data = np.append(data, x)
+                file_name += utterance[1] + '_'
+                transcript += utterance[2]
+
+            file_name = file_name[:-1]
+            transcript = transcript[:-1]
+
+        # save the new file and transcription
+        sf.write(cur_dir + file_name + '.flac', data, sr)
+        with open(cur_dir + file_name + '.txt', 'w') as txt:
+            if ALIGNED == True: txt.write(transcript + ' ' + alignment + '\n')
+            else: txt.write(transcript + '\n')
+            txt.close()
+            iteration += 1
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print("Incorrect number of parameters for the concatenation script")
+        print("Please specify the path (i.e. ~/data/LibriSpeech/dev-clean) to")
+        print("the dataset's root direcotory as the first parameter and the name")
+        print("of the destination folder that will contain the generated utterances.")
+        sys.exit(0)
+    else:
+        root = sys.argv[1]
+        dest = sys.argv[2]
+        if root[-1] != '/': root += '/'
+        if dest[-1] != '/': dest += '/'
+
+    dataset = load_dataset_structure(root)
+
+    # now create the destination directory
+    if os.path.exists(dest):
+        print('The specified destination folder is an existing file/directory')
+        sys.exit()
+    else:
+        try:
+            os.mkdir(dest)
+        except OSError:
+            print('Could not create destination directory %s' % dest)
+
+    # generate the dataset - TODO: multiprocessing
+    if MPROCESS == True:
+        gen1 = Process(target=generate_concatenations, args=(dataset, dest, 'a', N/4,))
+        gen2 = Process(target=generate_concatenations, args=(dataset, dest, 'b', N/4,))
+        gen3 = Process(target=generate_concatenations, args=(dataset, dest, 'c', N/4,))
+        gen4 = Process(target=generate_concatenations, args=(dataset, dest, 'd', N/4,))
+        gen1.start(); gen2.start(); gen3.start(); gen4.start()
+    else:
+        generate_concatenations(dataset, dest, n=N)
